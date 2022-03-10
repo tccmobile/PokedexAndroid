37d1658c563398a8cd5f33c503706630b5b4545a
tccmobile <William.smith@tulsacc.edu>
2022-03-10 12:07:04 -0600

    Fixed Alignments

--- app/src/main/java/com/example/pokedex/PokeDetail.kt
+++ app/src/main/java/com/example/pokedex/PokeDetail.kt
@@ -1,9 +1,12 @@
 package com.example.pokedex
 
+import android.content.res.Resources
+import android.util.DisplayMetrics
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -13,9 +16,14 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun pokeDetail(){
+    var sceneWidth = Resources.getSystem().displayMetrics.widthPixels
+    var sceneHeight = Resources.getSystem().displayMetrics.heightPixels
+    var sceneDepth = Resources.getSystem().displayMetrics.density
+
     Box(modifier = Modifier
         .fillMaxWidth()
         .fillMaxHeight()
@@ -36,18 +44,32 @@ fun pokeDetail(){
                     .wrapContentSize(Alignment.Center)
                     .fillMaxHeight(.7f)
                     .align(Alignment.BottomCenter)
-            ){
+            ) {
+                Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+                    Spacer(modifier = Modifier.padding(vertical = 25.dp))
+                    Text("#1", fontSize = 24.sp)
+                    Text("Bulbasaur", fontSize = 36.sp)
+                    TypeBox()
+                    Spacer(modifier = Modifier.padding(vertical = 15.dp))
+                    attribBox(modifier = Modifier.padding(20.dp))
 
+
+            }
             }
             Box(
                 modifier = Modifier
                     .align(Alignment.Center)
-                    .offset(0.dp, -150.dp)
+                    .offset(0.dp, -(sceneHeight / (sceneDepth * 5)).dp)
             ){
+
                 Image(painter = painterResource(id = R.drawable.bulb),
                 contentDescription = "Bulbasaur",
-                    modifier = Modifier.scale(1.3f) )
+                    modifier = Modifier.size(width = ((sceneWidth/sceneDepth)/4).dp,
+                        height = ((sceneHeight/sceneDepth)/4).dp ) )
+
             }
+
+
         }
     }
 }
@@ -59,14 +81,30 @@ fun detailPreview(){
 }
 
 @Composable
-fun attribBox(){
-    Row(){
-
+fun attribBox(modifier: Modifier){
+    Row(modifier = Modifier.padding(10.dp)){
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Row(verticalAlignment = Alignment.CenterVertically){
+                Text("0.7", fontSize = 24.sp)
+                Text("KG", modifier = Modifier.padding(2.dp),
+                color = Color.LightGray)
+            }
+            Text("Weight", color = Color.LightGray)
+        }
+        Spacer(modifier = Modifier.width(15.dp))
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Row(verticalAlignment = Alignment.CenterVertically){
+                Text("6.9", fontSize = 24.sp)
+                Text("METER", modifier = Modifier.padding(2.dp),
+                    color = Color.LightGray)
+            }
+            Text("Height", color = Color.LightGray)
+        }
     }
 }
 
 @Composable
 @Preview(showBackground = true)
 fun previewAttrib(){
-    attribBox()
+    attribBox(modifier = Modifier.padding(20.dp))
 }
